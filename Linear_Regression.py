b60d5fb559f6bec6677895104baafcca09c59206
Ashu <ashenafitm@gmail.com>
2019-07-13 12:42:50 +0200

    fitting the curve

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -3,4 +3,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
 import numpy as np
 
+#y=ax+b
+
+rng = np.random.RandomState(1)
+x = 10 * rng.rand(50)
+y = 2 * x - 5 + rng.randn(50)
+plt.scatter(x, y);
+
+
+#We can use Scikit-Learn's LinearRegression estimator to fit this data and construct the best-fit line
+
+from sklearn.linear_model import LinearRegression
+model = LinearRegression(fit_intercept=True)
+
+model.fit(x[:, np.newaxis], y)
+
+xfit = np.linspace(0, 10, 1000)
+yfit = model.predict(xfit[:, np.newaxis])
+
+plt.scatter(x, y)
+plt.plot(xfit, yfit);
+
+#printing slop and intercept
+
+print("Model slope:    ", model.coef_[0])
+print("Model intercept:", model.intercept_)
 
